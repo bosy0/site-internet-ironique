19a47225d7e5e36949f413355aae3e00040fcc80
Nathan BOSY <104134380+bosy0@users.noreply.github.com>
2023-03-29 01:11:25 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,503 @@
+(function() {
+	var mainHeader = document.getElementsByClassName('js-header');
+	if( mainHeader.length > 0 ) {
+		var trigger = mainHeader[0].getElementsByClassName('js-header__trigger')[0],
+			nav = mainHeader[0].getElementsByClassName('js-header__nav')[0];
+
+		var focusMenu = false;
+
+		trigger.addEventListener("click", function(event) {
+			event.preventDefault();
+			toggleNavigation(!Util.hasClass(nav, 'header__nav--is-visible'));
+		});
+
+		window.addEventListener('keyup', function(event){
+			if( (event.keyCode && event.keyCode == 27) || (event.key && event.key.toLowerCase() == 'escape' )) {
+				if(trigger.getAttribute('aria-expanded') == 'true' && isVisible(trigger)) {
+					focusMenu = trigger; 
+					trigger.click();
+				}
+			}
+			if( (event.keyCode && event.keyCode == 9) || (event.key && event.key.toLowerCase() == 'tab' )) {
+				if(trigger.getAttribute('aria-expanded') == 'true' && isVisible(trigger) && !document.activeElement.closest('.js-header')) trigger.click();
+			}
+		});
+
+		var resizingId = false;
+		window.addEventListener('resize', function() {
+			clearTimeout(resizingId);
+			resizingId = setTimeout(doneResizing, 500);
+		});
+
+		function doneResizing() {
+			if( !isVisible(trigger) && Util.hasClass(mainHeader[0], 'header--expanded')) toggleNavigation(false); 
+		};
+	}
+
+	function isVisible(element) {
+		return (element.offsetWidth || element.offsetHeight || element.getClientRects().length);
+	};
+
+	function toggleNavigation(bool) { 
+		Util.toggleClass(nav, 'header__nav--is-visible', bool);
+		Util.toggleClass(mainHeader[0], 'header--expanded', bool);
+		trigger.setAttribute('aria-expanded', bool);
+		if(bool) { 
+			nav.querySelectorAll('[href], input:not([disabled]), button:not([disabled])')[0].focus();
+		} else if(focusMenu) {
+			focusMenu.focus();
+			focusMenu = false;
+		}
+	};
+}());
+
+
+(function() {
+  var TextAnim = function(element) {
+    this.element = element;
+    this.wordsWrapper = this.element.getElementsByClassName(' js-text-anim__wrapper');
+    this.words = this.element.getElementsByClassName('js-text-anim__word');
+    this.selectedWord = 0;
+    this.loopInterval = parseFloat(getComputedStyle(this.element).getPropertyValue('--text-anim-pause'))*1000 || 1000;
+    this.transitionDuration = parseFloat(getComputedStyle(this.element).getPropertyValue('--text-anim-duration'))*1000 || 1000;
+    this.loop = (this.element.getAttribute('data-loop') && this.element.getAttribute('data-loop') == 'off') ? false : true;
+    this.wordInClass = 'text-anim__word--in';
+    this.wordOutClass = 'text-anim__word--out';
+    this.isClipAnim = Util.hasClass(this.element, 'text-anim--clip');
+    if(this.isClipAnim) {
+      this.animBorderWidth = parseInt(getComputedStyle(this.element).getPropertyValue('--text-anim-border-width')) || 2;
+      this.animPulseClass = 'text-anim__wrapper--pulse';
+    }
+    initTextAnim(this);
+  };
+
+  function initTextAnim(element) {
+    setSelectedWord(element);
+    if(element.isClipAnim) {
+      Util.addClass(element.wordsWrapper[0], element.animPulseClass);
+    }
+    loopWords(element);
+  };
+
+  function setSelectedWord(element) {
+    var selectedWord = element.element.getElementsByClassName(element.wordInClass);
+    if(selectedWord.length == 0) {
+      Util.addClass(element.words[0], element.wordInClass);
+    } else {
+      element.selectedWord = Util.getIndexInArray(element.words, selectedWord[0]);
+    }
+  };
+
+  function loopWords(element) {
+    if(!element.loop && element.selectedWord == element.words.length - 1) {
+      return;
+    }
+    var newWordIndex = getNewWordIndex(element);
+    setTimeout(function() {
+      if(element.isClipAnim) { 
+        switchClipWords(element, newWordIndex);
+      } else {
+        switchWords(element, newWordIndex);
+      }
+    }, element.loopInterval);
+  };
+
+  function switchWords(element, newWordIndex) {
+    Util.removeClass(element.words[element.selectedWord], element.wordInClass);
+    Util.addClass(element.words[element.selectedWord], element.wordOutClass);
+    Util.addClass(element.words[newWordIndex], element.wordInClass);
+    resetLoop(element, newWordIndex);
+  };
+
+  function resetLoop(element, newIndex) {
+    setTimeout(function() { 
+      Util.removeClass(element.words[element.selectedWord], element.wordOutClass);
+      element.selectedWord = newIndex;
+      loopWords(element);
+    }, element.transitionDuration);
+  };
+
+  function switchClipWords(element, newWordIndex) {
+    var startWidth =  element.words[element.selectedWord].offsetWidth,
+      endWidth = element.words[newWordIndex].offsetWidth;
+    
+
+    Util.removeClass(element.wordsWrapper[0], element.animPulseClass);
+    animateWidth(startWidth, element.animBorderWidth, element.wordsWrapper[0], element.transitionDuration, function() {
+      Util.removeClass(element.words[element.selectedWord], element.wordInClass);
+      Util.addClass(element.words[newWordIndex], element.wordInClass);
+      element.selectedWord = newWordIndex;
+
+      animateWidth(element.animBorderWidth, endWidth, element.wordsWrapper[0], element.transitionDuration, function() {
+        Util.addClass(element.wordsWrapper[0], element.animPulseClass);
+        loopWords(element);
+      });
+    });
+  };
+
+  function getNewWordIndex(element) {
+    var index = element.selectedWord + 1;
+    if(index >= element.words.length) index = 0;
+    return index;
+  };
+
+  function animateWidth(start, to, element, duration, cb) {
+    var currentTime = null;
+
+    var animateProperty = function(timestamp){  
+      if (!currentTime) currentTime = timestamp;         
+      var progress = timestamp - currentTime;
+      
+      var val = Math.easeInOutQuart(progress, start, to - start, duration);
+      element.style.width = val+"px";
+      if(progress < duration) {
+          window.requestAnimationFrame(animateProperty);
+      } else {
+        cb();
+      }
+    };
+  
+    element.style.width = start+"px";
+    window.requestAnimationFrame(animateProperty);
+  };
+
+  window.TextAnim = TextAnim;
+
+  var textAnim = document.getElementsByClassName('js-text-anim'),
+    reducedMotion = Util.osHasReducedMotion();
+  if( textAnim ) {
+    if(reducedMotion) return;
+    for( var i = 0; i < textAnim.length; i++) {
+      (function(i){ new TextAnim(textAnim[i]);})(i);
+    }
+  }
+}());
+
+(function() {
+  var StickyBackground = function(element) {
+    this.element = element;
+    this.scrollingElement = this.element.getElementsByClassName('sticky-hero__content')[0];
+    this.nextElement = this.element.nextElementSibling;
+    this.scrollingTreshold = 0;
+    this.nextTreshold = 0;
+    initStickyEffect(this);
+  };
+
+  function initStickyEffect(element) {
+    var observer = new IntersectionObserver(stickyCallback.bind(element), { threshold: [0, 0.1, 1] });
+    observer.observe(element.scrollingElement);
+    if(element.nextElement) observer.observe(element.nextElement);
+  };
+
+  function stickyCallback(entries, observer) {
+    var threshold = entries[0].intersectionRatio.toFixed(1);
+    (entries[0].target ==  this.scrollingElement)
+      ? this.scrollingTreshold = threshold
+      : this.nextTreshold = threshold;
+
+    Util.toggleClass(this.element, 'sticky-hero--media-is-fixed', (this.nextTreshold > 0 || this.scrollingTreshold > 0));
+  };
+
+
+  var stickyBackground = document.getElementsByClassName('js-sticky-hero'),
+    intersectionObserverSupported = ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype);
+  if(stickyBackground.length > 0 && intersectionObserverSupported) { 
+    for(var i = 0; i < stickyBackground.length; i++) {
+      (function(i){ 
+        if( Util.hasClass(stickyBackground[i], 'sticky-hero--overlay-layer') || Util.hasClass(stickyBackground[i], 'sticky-hero--scale')) new StickyBackground(stickyBackground[i]);
+      })(i);
+    }
+  }
+}());
+
+(function() {
+	var fxElements = document.getElementsByClassName('reveal-fx');
+	var intersectionObserverSupported = ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype);
+	if(fxElements.length > 0) {
+		if (Util.osHasReducedMotion() || !intersectionObserverSupported) {
+			fxRemoveClasses();
+			return;
+		}
+		if( fxDisabled(fxElements[0]) ) {
+			fxRevealAll();
+			return;
+		}
+
+		var fxRevealDelta = 120; 
+		
+		var viewportHeight = window.innerHeight,
+			fxChecking = false,
+			fxRevealedItems = [],
+			fxElementDelays = fxGetDelays(), 
+			fxElementDeltas = fxGetDeltas(); 
+		
+		
+		window.addEventListener('load', fxReveal);
+		window.addEventListener('resize', fxResize);
+		window.addEventListener('restartAll', fxRestart);
+
+		var observer = [];
+		initObserver();
+
+		function initObserver() {
+			for(var i = 0; i < fxElements.length; i++) {
+				observer[i] = new IntersectionObserver(
+					function(entries, observer) { 
+						if(entries[0].isIntersecting) {
+							fxRevealItemObserver(entries[0].target);
+							observer.unobserve(entries[0].target);
+						}
+					}, 
+					{rootMargin: "0px 0px -"+fxElementDeltas[i]+"px 0px"}
+				);
+	
+				observer[i].observe(fxElements[i]);
+			}
+		};
+
+		function fxRevealAll() { 
+			for(var i = 0; i < fxElements.length; i++) {
+				Util.addClass(fxElements[i], 'reveal-fx--is-visible');
+			}
+		};
+
+		function fxResize() { 
+			if(fxChecking) return;
+			fxChecking = true;
+			(!window.requestAnimationFrame) ? setTimeout(function(){fxReset();}, 250) : window.requestAnimationFrame(fxReset);
+		};
+
+		function fxReset() {
+			viewportHeight = window.innerHeight;
+			fxReveal();
+		};
+
+		function fxReveal() { 
+			for(var i = 0; i < fxElements.length; i++) {(function(i){
+				if(fxRevealedItems.indexOf(i) != -1 ) return; 
+				if(fxElementIsVisible(fxElements[i], i)) {
+					fxRevealItem(i);
+					fxRevealedItems.push(i);
+				}})(i); 
+			}
+			fxResetEvents(); 
+			fxChecking = false;
+		};
+
+		function fxRevealItem(index) {
+			if(fxElementDelays[index] && fxElementDelays[index] != 0) {
+				setTimeout(function(){
+					Util.addClass(fxElements[index], 'reveal-fx--is-visible');
+				}, fxElementDelays[index]);
+			} else {
+				Util.addClass(fxElements[index], 'reveal-fx--is-visible');
+			}
+		};
+
+		function fxRevealItemObserver(item) {
+			var index = Util.getIndexInArray(fxElements, item);
+			if(fxRevealedItems.indexOf(index) != -1 ) return; 
+			fxRevealItem(index);
+			fxRevealedItems.push(index);
+			fxResetEvents(); 
+			fxChecking = false;
+		};
+
+		function fxGetDelays() { 
+			var delays = [];
+			for(var i = 0; i < fxElements.length; i++) {
+				delays.push( fxElements[i].getAttribute('data-reveal-fx-delay') ? parseInt(fxElements[i].getAttribute('data-reveal-fx-delay')) : 0);
+			}
+			return delays;
+		};
+
+		function fxGetDeltas() { 
+			var deltas = [];
+			for(var i = 0; i < fxElements.length; i++) {
+				deltas.push( fxElements[i].getAttribute('data-reveal-fx-delta') ? parseInt(fxElements[i].getAttribute('data-reveal-fx-delta')) : fxRevealDelta);
+			}
+			return deltas;
+		};
+
+		function fxDisabled(element) { 
+			return !(window.getComputedStyle(element, '::before').getPropertyValue('content').replace(/'|"/g, "") == 'reveal-fx');
+		};
+
+		function fxElementIsVisible(element, i) { 
+			return (fxGetElementPosition(element) <= viewportHeight - fxElementDeltas[i]);
+		};
+
+		function fxGetElementPosition(element) { 
+			return element.getBoundingClientRect().top;
+		};
+
+		function fxResetEvents() { 
+			if(fxElements.length > fxRevealedItems.length) return;
+			window.removeEventListener('load', fxReveal);
+			window.removeEventListener('resize', fxResize);
+		};
+
+		function fxRemoveClasses() {
+			while(fxElements[0]) {
+				var classes = fxElements[0].getAttribute('class').split(" ").filter(function(c) {
+					return c.lastIndexOf('reveal-fx--', 0) !== 0;
+				});
+				fxElements[0].setAttribute('class', classes.join(" ").trim());
+				Util.removeClass(fxElements[0], 'reveal-fx');
+			}
+		};
+
+		function fxRestart() {
+      if (Util.osHasReducedMotion() || !intersectionObserverSupported || fxDisabled(fxElements[0])) {
+        return;
+      }
+      if(fxElements.length <= fxRevealedItems.length) {
+        window.addEventListener('load', fxReveal);
+        window.addEventListener('resize', fxResize);
+      }
+      for(var i = 0; i < observer.length; i++) {
+        if(observer[i]) observer[i].disconnect();
+      }
+      observer = [];
+      for(var i = 0; i < fxElements.length; i++) {
+        Util.removeClass(fxElements[i], 'reveal-fx--is-visible');
+      }
+      fxRevealedItems = [];
+      initObserver();
+    };
+	}
+}());
+
+(function() {
+  var StackCards = function(element) {
+    this.element = element;
+    this.items = this.element.getElementsByClassName('js-stack-cards__item');
+    this.scrollingFn = false;
+    this.scrolling = false;
+    initStackCardsEffect(this); 
+    initStackCardsResize(this); 
+  };
+
+  function initStackCardsEffect(element) { 
+    setStackCards(element); 
+		var observer = new IntersectionObserver(stackCardsCallback.bind(element), { threshold: [0, 1] });
+		observer.observe(element.element);
+  };
+
+  function initStackCardsResize(element) {
+    element.element.addEventListener('resize-stack-cards', function(){
+      setStackCards(element);
+      animateStackCards.bind(element);
+    });
+  };
+  
+  function stackCardsCallback(entries) { 
+    if(entries[0].isIntersecting) {
+      if(this.scrollingFn) return; 
+      stackCardsInitEvent(this);
+    } else {
+      if(!this.scrollingFn) return; 
+      window.removeEventListener('scroll', this.scrollingFn);
+      this.scrollingFn = false;
+    }
+  };
+  
+  function stackCardsInitEvent(element) {
+    element.scrollingFn = stackCardsScrolling.bind(element);
+    window.addEventListener('scroll', element.scrollingFn);
+  };
+
+  function stackCardsScrolling() {
+    if(this.scrolling) return;
+    this.scrolling = true;
+    window.requestAnimationFrame(animateStackCards.bind(this));
+  };
+
+  function setStackCards(element) {
+    element.marginY = getComputedStyle(element.element).getPropertyValue('--stack-cards-gap');
+    getIntegerFromProperty(element); 
+    element.elementHeight = element.element.offsetHeight;
+
+    var cardStyle = getComputedStyle(element.items[0]);
+    element.cardTop = Math.floor(parseFloat(cardStyle.getPropertyValue('top')));
+    element.cardHeight = Math.floor(parseFloat(cardStyle.getPropertyValue('height')));
+
+    element.windowHeight = window.innerHeight;
+
+    if(isNaN(element.marginY)) {
+      element.element.style.paddingBottom = '0px';
+    } else {
+      element.element.style.paddingBottom = (element.marginY*(element.items.length - 1))+'px';
+    }
+
+    for(var i = 0; i < element.items.length; i++) {
+      if(isNaN(element.marginY)) {
+        element.items[i].style.transform = 'none;';
+      } else {
+        element.items[i].style.transform = 'translateY('+element.marginY*i+'px)';
+      }
+    }
+  };
+
+  function getIntegerFromProperty(element) {
+    var node = document.createElement('div');
+    node.setAttribute('style', 'opacity:0; visbility: hidden;position: absolute; height:'+element.marginY);
+    element.element.appendChild(node);
+    element.marginY = parseInt(getComputedStyle(node).getPropertyValue('height'));
+    element.element.removeChild(node);
+  };
+
+  function animateStackCards() {
+    if(isNaN(this.marginY)) { 
+      this.scrolling = false;
+      return; 
+    }
+
+    var top = this.element.getBoundingClientRect().top;
+
+    if( this.cardTop - top + this.element.windowHeight - this.elementHeight - this.cardHeight + this.marginY + this.marginY*this.items.length > 0) { 
+      this.scrolling = false;
+      return;
+    }
+
+    for(var i = 0; i < this.items.length; i++) { 
+      var scrolling = this.cardTop - top - i*(this.cardHeight+this.marginY);
+      if(scrolling > 0) {  
+        var scaling = i == this.items.length - 1 ? 1 : (this.cardHeight - scrolling*0.05)/this.cardHeight;
+        this.items[i].style.transform = 'translateY('+this.marginY*i+'px) scale('+scaling+')';
+      } else {
+        this.items[i].style.transform = 'translateY('+this.marginY*i+'px)';
+      }
+    }
+
+    this.scrolling = false;
+  };
+
+  var stackCards = document.getElementsByClassName('js-stack-cards'),
+    intersectionObserverSupported = ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype),
+    reducedMotion = Util.osHasReducedMotion();
+    
+	if(stackCards.length > 0 && intersectionObserverSupported && !reducedMotion) { 
+    var stackCardsArray = [];
+		for(var i = 0; i < stackCards.length; i++) {
+			(function(i){
+        stackCardsArray.push(new StackCards(stackCards[i]));
+      })(i);
+    }
+    
+    var resizingId = false,
+      customEvent = new CustomEvent('resize-stack-cards');
+    
+    window.addEventListener('resize', function() {
+      clearTimeout(resizingId);
+      resizingId = setTimeout(doneResizing, 500);
+    });
+
+    function doneResizing() {
+      for( var i = 0; i < stackCardsArray.length; i++) {
+        (function(i){stackCardsArray[i].element.dispatchEvent(customEvent)})(i);
+      };
+    };
+	}
+}());
+
